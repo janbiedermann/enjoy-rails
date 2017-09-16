1f9a91f7722d44e134722d83f4220b108009ffee
Jan Biedermann <jan@kursator.de>
2017-09-16 14:04:49 +0200

    add some benchmarks

--- app/assets/javascripts/application.rb
+++ app/assets/javascripts/application.rb
@@ -1,12 +1,28 @@
 require 'rails-ujs'
-require 'turbolinks'
 require 'cable'
 
 require 'opal'
+
+def benchmark message
+  if `!!(console.time && console.timeEnd)`
+    `console.time(message)`
+    result = yield
+    `console.timeEnd(message)`
+  else
+    start = `performance.now()`
+    result = yield
+    finish = `performance.now()`
+    puts "#{message} in #{(finish - start).round(3)}ms"
+  end
+
+  result
+end
+
 require 'enjoy'
 
 class OlaComponent
   include Enjoy::Component::Mixin
+
   render do
     DIV { 'text' }
   end
@@ -14,6 +30,7 @@ end
 
 class TestComponent
   include Enjoy::Component::Mixin
+
   render(DIV) do
     DIV(class: 'ohmy')
     H1 { 'Überschrift ' }
@@ -28,4 +45,35 @@ class TestComponent
   end
 end
 
-Enjoy.start(TestComponent)
+class MasterComponent < Enjoy::Component
+  render do
+    1000.times do
+      TestComponent()
+    end
+  end
+end
+
+benchmark('app') { Enjoy.start(MasterComponent) }
+i = 0
+`i = document.getElementsByTagName("*").length`
+puts "rendered #{i} nodes"
+
+benchmark('app') { Enjoy.start(MasterComponent) }
+benchmark('app') { Enjoy.start(MasterComponent) }
+benchmark('app') { Enjoy.start(MasterComponent) }
+benchmark('app') { Enjoy.start(MasterComponent) }
+`i = document.getElementsByTagName("*").length`
+puts "rendered #{i} nodes"
+
+# benchmark('total') do
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# benchmark('app') { Enjoy.start(TestComponent) }
+# end
