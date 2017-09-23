516564da5ede89353d8ff3f007dd08514cb90148
Jan Biedermann <jan@kursator.de>
2017-09-24 01:43:57 +0200

    clarification, fixes

--- app/assets/javascripts/enjoy/parts/tags.rb
+++ app/assets/javascripts/enjoy/parts/tags.rb
@@ -9,9 +9,10 @@ module Enjoy
                      dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5
                      h6 head header hr html i iframe img input ins kbd keygen label legend li link
                      main map mark menu menuitem meta meter nav noscript object ol optgroup option
-                     output p param picture pre progress q rp rt ruby s samp script section select
+                     output p picture pre progress q rp rt ruby s samp script section select
                      small source span strong style sub summary sup table tbody td textarea tfoot th
                      thead time title tr track u ul var video wbr] +
+                  # TODO param taken out from above
                   # The SVG Tags
                   %w[circle clipPath defs ellipse g line linearGradient mask path pattern polygon polyline
                      radialGradient rect stop svg text tspan]
@@ -20,7 +21,7 @@ module Enjoy
         # we override Kernel.p and we define div hoping that it would not matter in this context
         # TODO: document p and div issue
         define_method(tag) do |*params, &block|
-          ::Enjoy::Parts::VNode.new(tag, self, nil, *params, &block)
+          ::Enjoy::Parts::VNode.new(tag, self, self.base_dom_node, *params, &block)
         end
         alias_method tag.upcase, tag
         const_set tag.upcase, tag
@@ -29,9 +30,11 @@ module Enjoy
       # use method_missing to look up component names in the form of "Foo(..)"
       # where there is no preceeding scope.
       def method_missing(name, *params, &block)
-        component = find_component(name)
-        return component.new('div', self, base_dom_node).render if component
-        Object.method_missing(name, *params, &block)
+        component_class = find_component(name)
+        return component_class.new('div', self, self.base_dom_node, *params).render if component_class
+        # @self_before_instance_eval.send(name, *params, &block) if @self_before_instance_eval && @self_before_instance_eval.respond_to?(name)
+        @component.send(name, *params, &block) if @component && @component.respond_to?(name)
+        # Object.method_missing(name, *params, &block)
       end
 
       # install methods with the same name as the component in the parent class/module
@@ -60,11 +63,11 @@ module Enjoy
       private
 
       def find_component(name)
-        component = lookup_const(name)
-        if component && !component.method_defined?(:render)
+        component_class = lookup_const(name)
+        if component_class && !component_class.method_defined?(:render)
           raise "#{name} does not appear to be a component."
         end
-        component
+        component_class
       end
 
       def lookup_const(name)
