b58d719a834ef19b39e7b27fee2ca3e537392f37
Jan Biedermann <jan@kursator.de>
2017-09-24 01:40:21 +0200

    params

--- app/assets/javascripts/enjoy/parts/dsl_instance_methods.rb
+++ app/assets/javascripts/enjoy/parts/dsl_instance_methods.rb
@@ -2,6 +2,10 @@ module Enjoy
   module Parts
     module DslInstanceMethods
       def params
+        @params_wrapper ||= self.class.params_wrapper.new(self)
+      end
+
+      def props
         @attributes
       end
     end
