c59d53cc19fc6416f078f5dfd16e558ba58cab04
Jan Biedermann <jan@kursator.de>
2017-09-24 01:44:37 +0200

    clarifications and fixes

--- app/assets/javascripts/enjoy/parts/v_node.rb
+++ app/assets/javascripts/enjoy/parts/v_node.rb
@@ -14,35 +14,33 @@ module Enjoy
         attr_accessor :attributes, :base_dom_node, :children, :component, :key, :node_name, :opts, :parent_dom_node, :parent_v_node
 
         def initialize(tag, parent_v_node = nil, parent_dom_node = nil, attributes = {}, &block)
-          @attributes = attributes || {}
+          @attributes = attributes ? attributes : {}
           @children = []
           @events = {}
           @key = @attributes[:key] if @attributes[:key]
           @node_name = tag
           @opts = {}
-          if parent_v_node
+          if parent_v_node && parent_v_node.respond_to?(:is_vnode?)
+            @component = parent_v_node.component
             @parent_v_node = parent_v_node
             @parent_v_node.children << self
           end
           @parent_dom_node = parent_dom_node
           internal_render(attributes, &block) if block_given?
+          # puts "VNode: s: #{self} nn: #{@node_name} pvn: #{@parent_v_node} pdn: #{@parent_dom_node} bdn: #{@base_dom_node} c: #{@component} o: #{@opts} e: #{@events} c: #{@children}"
         end
 
         def internal_render(attributes = nil, &block)
           @attributes = @attributes.merge(attributes) if attributes
-          if block_given?
-            res = instance_exec &block
-            return if res.respond_to?(:is_vnode?)
-            @children << res
-          end
-        end unless method_defined?(:internal_render)
+          res = instance_exec(&block)
+          return if res.respond_to?(:is_vnode?)
+          @children << res
+          # puts "VNode: s: #{self} nn: #{@node_name} pvn: #{@parent_v_node} pdn: #{@parent_dom_node} bdn: #{@base_dom_node} c: #{@component} o: #{@opts} e: #{@events} c: #{@children}"
+          self
+        end
 
         def is_vnode?
           true
-        end
-
-        def is_component?
-          false
         end
 
         def render(tag, params, &block)
